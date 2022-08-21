d5c75fb546dd47c646d48a7fa02d825ac8454e79
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-08-21 19:19:51 -0300

    Update FibonacciController.java

--- Exec5-Recursiva/src/Controller/FibonacciController.java
+++ Exec5-Recursiva/src/Controller/FibonacciController.java
@@ -7,11 +7,12 @@ public class FibonacciController {
 		}
 	
 	public int sequenciaFibonacci(int num) {
-		
+		//condição de parada quando o valor inicial for igual a 1 ou 2
 		if (num == 1 || num == 2) {
 			return 1;
 		} else {
 			return sequenciaFibonacci(num-1) + sequenciaFibonacci(num-2);
+			//nem froid explica, só sei que deu certo
 		}
 	}
 }
